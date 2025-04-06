d289c7bbf661dc1aa2112d2c21fd22a6751a958a
Shreyan Jain <shreyan.jain.9@outlook.com>
2025-04-05 19:04:33 -0700

    make it actually work

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 return {
     pds = "pds.shreyanjain.net",
     repo = "did:plc:bnqkww7bjxaacajzvu5gswdf",
-    post_collection = "com.whtwnd.blog.entry",
+    post_collection = "net.shreyanjain.blog.post",
     homepage_record = "net.shreyanjain.blog.homepage",
     richtext_lex = "net.shreyanjain.blog.richtext"
 }
